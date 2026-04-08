91648ca1292d170572a50270a6edb27b3ff3f888
SIZIANI Nassim <sizianinassim@gmail.com>
2026-04-08 20:23:17 +0200

    ajout de l'heritage dans la classe OrganizationElement

--- src/main/java/fr/uvsq/hal/pglp/patterns/OrganizationElement.java
+++ src/main/java/fr/uvsq/hal/pglp/patterns/OrganizationElement.java
@@ -1,10 +1,12 @@
 package fr.uvsq.hal.pglp.patterns;
 
+import java.io.Serializable;
+
 /**
  * La classe <code>OrganizationElement</code> représente un élément d'une organisation.
  *
  * @author hal
  * @version 2022
  */
-public interface OrganizationElement {
+public interface OrganizationElement extends Serializable {
 }
